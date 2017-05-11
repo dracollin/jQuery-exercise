ac24850589deda0496548dca19b26397990a9514
Pedro Collin <pedrocollin89@gmail.com>
2017-05-11 17:44:41 -0300

    add2

--- exercise.js
+++ exercise.js
@@ -1,7 +1,17 @@
 $("div").css("background", "purple");
 
+$(".clickme").css("background", "red");
+
 $("div.highlight").css("width", "200px");
 
 $("#third").css("border", "4px solid orange");
 
 $("div:first-of-type").css("color", "pink");
+
+//funcao do botao
+$("#button").on("click", function(){
+ $('.clickme').slideToggle(0050, function(){
+ 	$(this).add(); //remover ou adicionar
+ 	$("#button").attr('value', 'Bring me back!!');//altera a mensagem do botao
+ });
+});
